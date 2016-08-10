86950319ca7e017646256bb54b47b64a7aaacb1b
OTI <maciej@t-media.pl>
2016-08-10 13:56:25 +0200

    10 Seeds

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -7,37 +7,30 @@
  */
 
 class UserTableSeeder extends Seeder {
-    
-    
-    
+
     public function run() {
-    
+
+
 
         
-         \DB::beginTransaction();
-        
-        try{
-            
-       
-        
-        \DB::table('users')->truncate();
-        
-        for ($i = 0; $i < 5; $i++) {
-            
-            $user = User::create([
-                        'name' => str_random(5),
-                        'surname' => str_random(5),
-                        'username' => 'user'.$i,
-                        'password' => hash('sha512','root')
-                    ]);
-        
-         };
-            
-       
-    }catch (Exception $ads) 
-    {
-        DB::rollback();
+
+        try {
+        //\DB::beginTransaction();
+
+        //\DB::table('users')->truncate();
+
+            for ($i = 0; $i < 10; $i++) {
+
+                $user = User::create([
+                            'name' => str_random(5),
+                            'surname' => str_random(5),
+                            'username' => 'user' . $i,
+                            'password' => hash('sha512', 'surname')
+                ]);
+            };
+        } catch (Exception $ads) {
+           // DB::rollback();
+        }
     }
 
-}
 }
